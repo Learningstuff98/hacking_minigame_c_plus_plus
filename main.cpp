865f6158b18e5cc8235be8fc70af9d35863378c4
Andy Strube <andystrube64@gmail.com>
2024-03-11 19:51:28 -0700

    Basic structure is set up

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+std::string get_desired_operation() {
+  std::cout << "Please select one of the following options:" << std::endl;
+  std::cout << "start or exit." << std::endl;
+  std::string desired_operation {""};
+  std::cin >> desired_operation;
+  return desired_operation;
+};
+
 int main() {
-  std::cout << "Hello World" << std::endl; 
+  std::cout << "Hello and welcome to my basic C++ hacking minigame!" << std::endl;
+  std::string desired_operation {get_desired_operation()};
+  while(desired_operation != "exit") {
+    if(desired_operation == "start") {
+      std::cout << "START MATCH STUFF GOES HERE." << std::endl;
+      desired_operation = get_desired_operation();
+    } else {
+      std::cout << "ERROR! ERROR! INVALID SELECTION!" << std::endl;
+      desired_operation = get_desired_operation();
+    }
+  };
+  std::cout << "Program shutting down. Goodbye.";
   return 0;
 };
